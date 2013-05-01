76f2f971b7f60b71310daa2ca56f3ffca2c15ccd
Tobias Zeising <tobias.zeising@aditu.de>
2013-05-01 17:52:21 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<?PHP
+
+$sshUser = 'youruser';
+$sshPassword = 'yourpass';
+$sshHost = 'yourhost';
+$base = "/www/htdocs/<ftpuser>/";
+$backupDir = "backup/";
+
+$dbs = array(
+    array(
+        "name"   => "wordpress",
+        "dbname" => "d1234567",
+        "passwd" => "secret",
+        "dir"    => "wordpress"
+    ),
+    array(
+        "name"   => "website",
+        "dbname" => "d321",
+        "passwd" => "secret",
+        "dir"    => "website/www"
+    ),
+    array(
+        "name"   => "selfoss",
+        "dir"    => "website/selfoss"
+    ),
+    array(
+        "name"   => "database123",
+        "dbname" => "d987324",
+        "passwd" => "secret",
+    )
+);
+
+
+// ssh connection
+include('Net/SSH2.php');
+
+$ssh = new Net_SSH2($sshHost);
+if(!$ssh->login($sshUser, $sshPassword))
+    die('ssh login failed');
+
+
+$baseWithoutTrailingSlash = substr($base, 1);
+foreach($dbs as $db) {
+    echo $db["name"] . "<br />";
+    ob_flush();
+
+    // directory for backup
+    $dir = "";
+    if(isset($db["dir"]) && strlen(trim($db["dir"]))>0) {
+        $dir = $baseWithoutTrailingSlash . $db["dir"];
+    }
+
+    // backup database
+    $sql = "";
+    if(isset($db["dbname"]) && strlen(trim($db["dbname"]))>0) {
+        $db_name = $db["dbname"];
+        $db_passwd = $db["passwd"];
+        $sql_file = $db["name"] . "_" . date('Y.m.d') . ".sql";
+        $sql = $baseWithoutTrailingSlash . $backupDir . $sql_file;
+        echo $ssh->exec("mysqldump -u " . $db_name . " -p'" . $db_passwd . "' --allow-keywords --add-drop-table --complete-insert --quote-names " . $db_name . " > " . $base . $backupDir . $sql_file);
+    }
+    
+    // generate tar.gz file
+    $tar_file = $base . $backupDir . $db["name"] . "_" . date('Y.m.d') . ".tar.gz";
+    $result = $ssh->exec("tar cfz " . $tar_file . " -C / " . $dir . " " . $sql);
+    if(strlen(trim($result))>0)
+        echo $result . "<br />\n";
+    
+    // delete temporarily database dump
+    if(strlen($sql)>0 && strlen(trim($base))>4 && strlen(trim($sql_file))>4)
+        echo $ssh->exec("rm " . $base . $backupDir . $sql_file);
+}
+
+echo "finished";
