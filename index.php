4aa49177c155f24d8c20bd5294305234d26b695d
Tobias Zeising <tobias.zeising@aditu.de>
2016-06-12 19:47:37 +0200

    fix db bug

--- index.php
+++ index.php
@@ -40,9 +40,9 @@ if(!$ssh->login($sshUser, $sshPassword))
 
 
 $baseWithoutTrailingSlash = substr($base, 1);
+$str = "";
 foreach($dbs as $db) {
     echo $db["name"] . "<br />";
-    ob_flush();
 
     // directory for backup
     $dir = "";
@@ -57,18 +57,20 @@ foreach($dbs as $db) {
         $db_passwd = $db["passwd"];
         $sql_file = $db["name"] . "_" . date('Y.m.d') . ".sql";
         $sql = $baseWithoutTrailingSlash . $backupDir . $sql_file;
-        echo $ssh->exec("mysqldump -u " . $db_name . " -p'" . $db_passwd . "' --allow-keywords --add-drop-table --complete-insert --quote-names " . $db_name . " > " . $base . $backupDir . $sql_file);
+        $str = $str . "mysqldump --user=" . $db_name . " --password=" . $db_passwd . " --allow-keywords --add-drop-table --complete-insert --quote-names " . $db_name . " > " . $base . $backupDir . $sql_file . "\n";
     }
-    
+	
     // generate tar.gz file
-    $tar_file = $base . $backupDir . $db["name"] . "_" . date('Y.m.d') . ".tar.gz";
-    $result = $ssh->exec("tar cfz " . $tar_file . " -C / " . $dir . " " . $sql);
+    $tar_file = $base . $backupDir . date('Y.m.d') . "_" . $db["name"] . ".tar.gz";
+    $str = $str . "tar cfz " . $tar_file . " -C / " . $dir . " " . $sql . "\n";
     if(strlen(trim($result))>0)
         echo $result . "<br />\n";
     
     // delete temporarily database dump
     if(strlen($sql)>0 && strlen(trim($base))>4 && strlen(trim($sql_file))>4)
-        echo $ssh->exec("rm " . $base . $backupDir . $sql_file);
+        $str = $str . "rm " . $base . $backupDir . $sql_file . "\n";
 }
+
+$ssh->exec($str);
 
 echo "finished";
